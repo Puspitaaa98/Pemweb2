a6db6a6f319e7e2efc3b242e8d6b7e5bb94b8fbf
Puspitaaa98 <puspitasarri28@gmail.com>
2024-03-04 16:40:34 +0700

    yugas pekan 2

--- pertemuan 2/proses_nilai.php
+++ pertemuan 2/proses_nilai.php
@@ -15,6 +15,27 @@ if ($nilai_total > 55) {
     $status = 'Tidak Lulus';
 }
 
+//predikat
+if($nilai_total >= 0 && $nilai_total <= 35) {
+    $grade = "E";
+    $predikat = "Sangat Kurang";
+} else if($nilai_total >= 36 && $nilai_total <=55) {
+    $grade = "D";
+    $predikat = "Kurang";
+} else if($nilai_total >= 56 && $nilai_total <= 69) {
+    $grade = "C";
+    $predikat = "Cukup";
+} else if($nilai_total >= 70 && $nilai_total <=84) {
+    $grade = "B";
+    $predikat = "Memuaskan";
+} else if($nilai_total >= 85 && $nilai_total <=100) {
+    $grade = "A";
+    $predikat = "Sangat Memuaskan";
+} else {
+    $grade = "I";
+    $predikat = "";
+}
+
 // tampilan hasil form
 echo "Nama Lengkap: $nama";
 echo "<br> Mata Kuliah: $matkul";
@@ -23,3 +44,5 @@ echo "<br> Nilai UAS: $uas";
 echo "<br> Nilai Tugas: $tugas";
 echo "<br> Nilai Total: $nilai_total";
 echo "<br> Status: $status";
+echo "<br> Grade: $grade";
+echo "<br> Predikat: $predikat";
